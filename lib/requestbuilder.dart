cdc642e9c835b269b588f1f570f522675e032f33
Mohamed EssamEldin <Mohamed.EssamEldin@mdi.com.eg>
2025-09-27 23:19:27 +0300

    fix

--- lib/requestbuilder.dart
+++ lib/requestbuilder.dart
@@ -4,9 +4,9 @@ import 'flutter_dio_network_manager.dart';
 // Files
 class UploadFile {
   List<int>? bytes;
-  String? path, name;
+  String? path, name, key;
 
-  UploadFile({this.bytes, this.path, this.name});
+  UploadFile({this.bytes, this.path, this.name, this.key = 'file'});
 }
 
 // Builder for BaseClientGenerator
@@ -23,11 +23,53 @@ class RequestBuilder extends BaseClientGenerator {
     'Access-Control-Allow-Headers': 'Content-Type',
     'Access-Control-Allow-Credentials': 'true',
   };
-
   RequestBuilder setMultipart(
+    Map<String, dynamic> body,
+    UploadFile file,
+  ) {
+    _header?['Content-Type'] = 'multipart/form-data';
+
+    final formDataMap = <String, dynamic>{};
+    formDataMap.addAll(body);
+
+    if (file.bytes != null) {
+      formDataMap[file.key!] =
+          MultipartFile.fromBytes(file.bytes!, filename: file.name);
+    } else if (file.path != null) {
+      formDataMap[file.key!] =
+          MultipartFile.fromFileSync(file.path!, filename: file.name);
+    }
+
+    _body = FormData.fromMap(formDataMap);
+    return this;
+  }
+
+  RequestBuilder setMultipartFiles(
     Map<String, dynamic> body,
     List<UploadFile> files,
   ) {
+    _header?['Content-Type'] = 'multipart/form-data';
+
+    final formDataMap = <String, dynamic>{};
+    formDataMap.addAll(body);
+
+    for (var file in files) {
+      if (file.bytes != null) {
+        formDataMap[file.key!] =
+            MultipartFile.fromBytes(file.bytes!, filename: file.name);
+      } else if (file.path != null) {
+        formDataMap[file.key!] =
+            MultipartFile.fromFileSync(file.path!, filename: file.name);
+      }
+    }
+
+    _body = FormData.fromMap(formDataMap);
+    return this;
+  }
+
+  RequestBuilder setMultipartFilesOneKey(
+      Map<String, dynamic> body, List<UploadFile> files,
+      {String key = 'file'}) {
     _header?['Content-Type'] = 'multipart/form-data';
 
     final formDataMap = <String, dynamic>{};
@@ -47,7 +89,7 @@ class RequestBuilder extends BaseClientGenerator {
       }
     }
 
-    formDataMap['file'] = multipartFiles;
+    formDataMap[key] = multipartFiles;
 
     _body = FormData.fromMap(formDataMap);
     return this;
